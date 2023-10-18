2db4d4329d376e40c669ff9a4ae72b3fc9b7ef51
II novbe <II novbe@DESKTOP-4EOUDP3>
2023-10-18 18:09:02 +0400

    Add project files.

---
+++ Week-enum/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Week_enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weekdays[] days = (Weekdays[])Enum.GetValues(typeof(Weekdays));
+            foreach (Weekdays day in days)
+            {
+                Console.WriteLine(day);
+            }
+            Console.WriteLine("please enter your name");
+            string name= Console.ReadLine();
+            Console.WriteLine("please enter your surname");
+            string surname = Console.ReadLine();
+            Console.WriteLine("please enter your birtday");
+            int birthday=Convert.ToInt32( Console.ReadLine());
+            User user = new User(name, surname, days[birthday-1]);
+
+        }
+    }
+}
---
+++ Week-enum/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_enum
+{
+    internal class User
+    {
+        public string Name{ get; set; }
+        public string Surname{ get; set; }
+        public Weekdays BirthDay { get; set; }
+
+        public User(string Name ,string Surname , Weekdays BirthDay)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.BirthDay = BirthDay;
+        }
+    }
+}
